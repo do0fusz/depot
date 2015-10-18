9ecb1f89b8a5adab5aac1e71eb30525112cf911a
do0fusz <do0fusz@hotmail.com>
2015-10-18 22:09:38 +0200

    adding tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'store#index', as: :store
+
   resources :products
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
