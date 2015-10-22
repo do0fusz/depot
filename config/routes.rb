6d99879c7bf066d491da1940c5bfa80589310630
do0fusz <do0fusz@hotmail.com>
2015-10-22 15:55:36 +0200

    rake test:prepare doesn't setup the user database, such strange

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+
+  get 'admin' => 'admin#index'
+  controller :sessions do 
+    get 'login' => :new 
+    post 'login' => :create 
+    get 'logout' => :destroy 
+  end
+
   resources :users
   resources :orders
   resources :line_items
   resources :carts
+  
   resources :products do 
     get :who_bought, on: :member 
   end
