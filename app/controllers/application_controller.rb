cc7adb48db6df637f896178c2af5aba2ae6ab5d7
do0fusz <do0fusz@hotmail.com>
2015-10-23 22:43:15 +0200

    224, vim is pretty neat, but hard..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,28 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authorize  
+  before_action :set_i18n_internationale_from_params 
+
 
   protected 
-	  def authorize 
+	  
+    def set_i18n_internationale_from_params
+      if params[:locale]  
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.local = params[:locale]
+        else
+          flash.now[:notice] = 
+            "#{params[:locale]} translation not available"
+            logger.error flash.now[:notice]
+        end
+      end
+    end
+
+    def default_url_options
+      {locale: I18n.locale }
+    end
+    
+    def authorize 
 	  	unless User.find_by(id: session[:user_id])
 	  		redirect_to store_url, notice: "please log in"
 	  	end
